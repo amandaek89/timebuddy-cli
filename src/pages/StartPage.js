beef90ce855e834baf0423de3871c50610797967
amandaek89 <145875411+amandaek89@users.noreply.github.com>
2024-12-10 13:22:10 +0100

    the panels in startpage are working again

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -41,25 +41,27 @@ const StartPage = () => {
     return (
         <div className="start-page">
             {/* Header */}
-            <HeaderAuthenticated/> {/* Lägg till headern överst */}
+            <HeaderAuthenticated /> {/* Lägg till headern överst */}
 
-            <div className="left-panel">
-                <div className="clock">
-                    <p>{formattedTime}</p>
+            <div className="content">
+                <div className="left-panel">
+                    <div className="clock">
+                        <p>{formattedTime}</p>
+                    </div>
+                    <div className="date-info">
+                        <p className="date">{formattedDate}</p> {/* Datum */}
+                    </div>
+                    <div className="week-number">
+                        <p>Vecka: {weekNumber}</p> {/* Veckonummer */}
+                    </div>
                 </div>
-                <div className="date-info">
-                    <p className="date">{formattedDate}</p> {/* Datum */}
-                </div>
-                <div className="week-number">
-                    <p>Vecka: {weekNumber}</p> {/* Veckonummer */}
-                </div>
-            </div>
 
-            <div className="right-panel">
-                <DayView selectedDate={selectedDate}/> {/* Skicka valfritt datum till DayView */}
+                <div className="right-panel">
+                    <DayView selectedDate={selectedDate}/> {/* Skicka valfritt datum till DayView */}
+                </div>
             </div>
         </div>
-);
+    );
 };
 
 export default StartPage;
